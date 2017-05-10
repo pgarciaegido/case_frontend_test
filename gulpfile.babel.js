e6c73d140bdec029858776c15d3496c6c0ad9d44
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-05-10 18:50:07 +0200

    Setting up dev enviroment: SCSS and watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,20 @@ import gulp from 'gulp';
 import webpack from 'webpack';
 import gulpWebpack from 'webpack-stream';
 import { argv } from 'yargs';
+import sass from 'gulp-sass';
 
+
+// Bundles CSS
+gulp.task('styles', () => {
+
+  let unCompressed = sass().on('error', sass.logError);
+  let compressed = sass({ outputStyle: 'compressed'})
+                       .on('error', sass.logError);
+
+  return gulp.src('app/scss/app.scss')
+    .pipe(argv.production ? compressed : unCompressed)
+    .pipe(gulp.dest('./dist/css'))
+})
 
 // Bundles JS
 gulp.task('scripts', () => {
@@ -35,7 +48,7 @@ gulp.task('scripts', () => {
     plugins: [new webpack.optimize.UglifyJsPlugin()]
   }
   // If it has --production flag, minify.
-  return gulp.src('app/app.js')
+  return gulp.src('app/js/app.js')
     .pipe(argv.production ? gulpWebpack(prod, webpack) : gulpWebpack(dev, webpack))
     .pipe(gulp.dest('./dist'))
 })
@@ -43,5 +56,9 @@ gulp.task('scripts', () => {
 
 // Watch changes in files
 gulp.task('watch', () => {
-  gulp.watch('app/**/*.js', ['scripts'])
+  gulp.watch('app/js/**/*.js', ['scripts'])
+  gulp.watch('app/scss/**/*.scss', ['styles'])
 })
+
+// Bundles everything
+gulp.task('build', ['scripts', 'styles'])
