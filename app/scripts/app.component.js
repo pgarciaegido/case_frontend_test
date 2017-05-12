1e6bfc3ea20d9bf633cb187b75d7a088e4cf7b5a
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-05-13 01:47:42 +0200

    Albums and posts Component logics. Needs to refactor

--- app/scripts/app.component.js
+++ app/scripts/app.component.js
@@ -1,10 +1,6 @@
 export const AppComponent = {
   template: `<app-header></app-header>
-             <ng-outlet></ng-outlet>
-             <display-album></display-album>
-             <display-post></display-post>
-             <display-albums-posts></display-albums-posts><br>
-             <charts></charts>`,
+             <ng-outlet></ng-outlet>`,
   $routeConfig: [
     { path: '/', component: 'usersList', name: 'List' },
     { path: '/user/:id', component: 'displayAlbumsPosts', name: 'Display' },
--- app/scripts/components/display/display-albums-posts/display-albums-posts.component.js
+++ app/scripts/components/display/display-albums-posts/display-albums-posts.component.js
@@ -1,6 +1,29 @@
 export const DisplayAlbumsPosts = {
-  template: 'I will display albums and posts!!!!',
+  templateUrl: 'display-albums-posts.template.html',
   bindings: { },
   controllerAs: 'model',
-  controller: function() { }
+  controller: function($http) {
+    let model = this;
+
+    model.albums = [];
+    model.posts = [];
+
+    model.userId;
+
+    model.$routerOnActivate = function (next, previous) {
+      model.userId = next.params.id;
+
+      $http.get('http://jsonplaceholder.typicode.com/posts?userId=' + model.userId)
+        .then((response) => {
+          console.log(response.data);
+          model.posts = response.data;
+        })
+
+      $http.get('http://jsonplaceholder.typicode.com/albums?userId=' + model.userId)
+        .then((response) => {
+          console.log(response.data);
+          model.albums = response.data;
+        })
+    }
+  }
 }
