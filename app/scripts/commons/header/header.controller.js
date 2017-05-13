03ff1adc4178da59af41d5067a9e2499fe751971
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-05-13 22:00:33 +0200

    Responding to changes in browser url to check if search should be general or specific for user

--- app/scripts/commons/header/header.controller.js
+++ app/scripts/commons/header/header.controller.js
@@ -2,13 +2,28 @@ export default function HeaderController($location) {
   let model = this;
 
   model.searchText;
-  model.$onInit = function() {
-    // Searchs on clicking enter on the input
-    model.searchEnter = function(event) {
-      if(event.keyCode === 13) {
-        model.path = '/search/' + model.searchText;
-        $location.path(model.path);
-      }
+  model.userSearch = false;
+  model.userId;
+
+  // Searchs on clicking enter on the input
+  model.searchEnter = function(event) {
+    if(event.keyCode === 13) {
+      model.path = '/search/' + model.searchText;
+      $location.path(model.path);
+    }
+  }
+
+  // Executes when browser url changes.
+  window.onhashchange = function() {
+    let url = window.location.href;
+    let isUserRegex = /user/g;
+    let userIdRegex = /user\/([0-9]+)/;
+
+    // If url contains user, activates userSearch.
+    if(isUserRegex.test(url)) {
+      model.userSearch = true;
+      // Gets user id
+      model.userId = url.match(userIdRegex)[1];
     }
   }
 }
