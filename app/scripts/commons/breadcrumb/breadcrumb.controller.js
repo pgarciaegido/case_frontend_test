431f800d2abb83cd801a14a3036a306dab9bb8b9
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-05-15 03:14:28 +0200

    Logics on Displaying breadcrumbs dinamically depending on url

--- app/scripts/commons/breadcrumb/breadcrumb.controller.js
+++ app/scripts/commons/breadcrumb/breadcrumb.controller.js
@@ -7,10 +7,22 @@ export default function BreadcrumbController(HttpRequestsService,
 
   // If not working, make ajax call to get it from API.
 
+  model.url = $location.path();
   // Getting userId from url.
-  let url = $location.path();
   let userIdRegex = /user\/([0-9]+)/;
-  model.userId = url.match(userIdRegex)[1];
+  model.userId = model.url.match(userIdRegex)[1];
+
+  model.mainMenuBC;
+  model.albumBC;
+  model.postBC;
+
+  // Checking if path contains album or post to render appropiate message
+  model.pathContains = function () {
+    if (model.url.indexOf('album') !== -1) return model.albumBC = true;
+    else if (model.url.indexOf('post') !== -1) return model.postBC = true;
+    else return model.mainMenuBC = true;
+
+  }()
 
   if (!model.userName) {
     let path = '/users?id=' + model.userId;
